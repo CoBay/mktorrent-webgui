fb25c50cc08d8173a562b4d1582d794b5fe73ebd
Co Bay <57278521+CoBay@users.noreply.github.com>
2019-11-02 16:57:35 -0600

    Organizing

---
+++ app/Lib/Autoloader.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace CoBay\mktorrent_webgui\Lib;
+
+class Autoloader {
+	
+	private	 static		$_root;
+	
+	
+	
+	public static function Init(){
+		self::$_root = \CoBay\mktorrent_webgui\ROOT;
+		
+		spl_autoload_register(['self', 'Load']);
+	}
+	public static function Load( string $classname):bool {
+		$fp = self::$_root . str_replace(['CoBay\\mktorrent_webgui\\','\\'], ['', DIRECTORY_SEPARATOR] , $classname) . '.php';
+
+		if( file_exists( $fp ) ){
+			require $fp;
+			return true;
+		}
+		
+		return false;
+	}
+}
