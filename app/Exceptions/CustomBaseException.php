72b750006d1a5b7f41f0a53169f318f435a0c5da
Co Bay <57278521+CoBay@users.noreply.github.com>
2019-11-06 20:27:34 -0700

    Progress

--- app/Exceptions/CustomBaseException.php
+++ app/Exceptions/CustomBaseException.php
@@ -11,20 +11,18 @@ class CustomBaseException extends \Exception {
 	
 	public function logMsg():string{
 		return  get_class($this) . ' Exception Caught: ' . PHP_EOL .
-				self::TAB . $this->getMessage() . PHP_EOL .
-				self::TAB . $this->getFile() . ' [' . $this->getLine() .' ]' . PHP_EOL .
-				$this->traceAsString() . PHP_EOL;
+			self::TAB . $this->getMessage() . PHP_EOL .
+			self::TAB . $this->getFile() . ' [' . $this->getLine() .' ]' . PHP_EOL .
+			$this->traceAsString() . PHP_EOL;
 	}
 	public function displayMsg():string{
-		$return = PHP_EOL .  get_class($this) . ' Exception Caught:' . PHP_EOL . '<br /><br />' . PHP_EOL .
+		return  PHP_EOL .  get_class($this) . ' Exception Caught:' . PHP_EOL . '<br /><br />' . PHP_EOL .
 			self::TABS3 . '<h3>' . $this->getMessage() . '</h3>' . PHP_EOL .
 			self::TABS3 . '<br /><br />' . PHP_EOL .
 			self::TABS3 . 'Thrown at:<br />' . PHP_EOL .
 			self::TABS3 . 'File: <i>' . $this->getFile() . '</i><br />' . PHP_EOL .
-			self::TABS3 . 'Line: <b>' . $this->getLine() . '</b><br />' . PHP_EOL;
-		$return .= '<hr>';
-		
-		$return .= PHP_EOL . '<pre>' . $this->traceAsString() . '<pre>' . PHP_EOL;
-		return $return;		
+			self::TABS3 . 'Line: <b>' . $this->getLine() . '</b><br />' . PHP_EOL .
+			self::TABS3 . '<hr>' . PHP_EOL .
+			self::TABS3 . '<pre>' . $this->traceAsString() . '<pre>' . PHP_EOL;
 	}
 }
